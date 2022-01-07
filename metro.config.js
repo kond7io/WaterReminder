80def961ebc4cf411b303bb085df99af7003f0dc
konra <konrad.walentek@gmail.com>
2022-01-07 23:06:55 +0100

    feat: add short imports

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,24 @@
  *
  * @format
  */
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: {assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx', 'svg'],
+    },
+  };
+})();
