c7841eed9c51a95d1ef84143d79ac010fc85f958
konra <konrad.walentek@gmail.com>
2022-01-07 22:28:08 +0100

    feat: add deviceDimensions and componentsDimensions functions

---
+++ src/utils/hooks/constants/componentsDimensions.ts
@@ -0,0 +1,13 @@
+import {
+  windowHeight,
+  windowWidth,
+} from '../../../utils/hooks/constants/deviceDimension';
+
+export const buttonDimensions = {
+  width: windowWidth * 0.35,
+  height: windowWidth * 0.35,
+};
+
+export const iconDimensions = {
+  size: windowWidth * 0.1,
+};
